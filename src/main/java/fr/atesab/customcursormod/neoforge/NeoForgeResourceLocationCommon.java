b22635dbd789d8ee9ddf7b3ce9b52e2a586cd58b
xiaoliziawa <3682822069@qq.com>
2025-11-01 18:13:58 +0800

    Update NeoForgeResourceLocationCommon.java

--- src/main/java/fr/atesab/customcursormod/neoforge/NeoForgeResourceLocationCommon.java
+++ src/main/java/fr/atesab/customcursormod/neoforge/NeoForgeResourceLocationCommon.java
@@ -38,7 +38,6 @@ public class NeoForgeResourceLocationCommon extends ResourceLocationCommon {
             bindTexture();
         }
         RenderSystem.setShaderTexture(0, textureView);
-        // 设置当前纹理给自定义渲染管道
         NeoForgeGuiUtils.setCurrentTexture(resource);
     }
 
